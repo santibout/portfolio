18b5660613aa9c597753a215e97e529ae9eab514
santibout <santibout@yahoo.com>
2020-09-07 12:21:11 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.889dc4596b2dc7e6c50aa79f276ec6d9.js"
+  "/portfolio/precache-manifest.a751744025160bf9898a2ed487461841.js"
 );
 
 self.addEventListener('message', (event) => {
