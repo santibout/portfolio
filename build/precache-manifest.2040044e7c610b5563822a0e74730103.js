ae7d5cdba3b6e1e49b201e2230b513b6dc195b33
santibout <santibout@yahoo.com>
2020-09-06 22:26:57 -0700

    set up gh-pages

---
+++ build/precache-manifest.2040044e7c610b5563822a0e74730103.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b066fddc93685ee8687e42d4895489f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "17761bd431bfc691e661",
+    "url": "/portfolio/static/css/main.c070e1ea.chunk.css"
+  },
+  {
+    "revision": "a27e71190ccc24ca3539",
+    "url": "/portfolio/static/js/2.01853480.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/portfolio/static/js/2.01853480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17761bd431bfc691e661",
+    "url": "/portfolio/static/js/main.a8a45daf.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c679d8a082b3a49c2fca0951df473c7c",
+    "url": "/portfolio/static/media/Lights-Out.c679d8a0.png"
+  },
+  {
+    "revision": "95c7767da9fc98de8ab6e027ed5718db",
+    "url": "/portfolio/static/media/download.95c7767d.png"
+  },
+  {
+    "revision": "01bab7d2bf1462b528c132cca27c6c4f",
+    "url": "/portfolio/static/media/omnifood.01bab7d2.png"
+  },
+  {
+    "revision": "3ea43197d5809b87348a6e7efa4fe8c3",
+    "url": "/portfolio/static/media/resume-snippit.3ea43197.png"
+  },
+  {
+    "revision": "c4fe0a4dc0e54b4fa3cbb318c24b6f78",
+    "url": "/portfolio/static/media/santibout_resume.c4fe0a4d.pdf"
+  }
+]);
