d43c339612ebb8de8c51150a72b9d7b4681b4e14
santibout <santibout@yahoo.com>
2020-09-07 13:20:58 -0700

    changed about mad and map margin top some more

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.39f7999686ca540dd4c90cb25ed75dbb.js"
+  "/portfolio/precache-manifest.39bedcb9be0e40e4e87feacfde5947b5.js"
 );
 
 self.addEventListener('message', (event) => {
