997f55370799e507945bc1dc25877b88374998bf
santibout <santibout#@yahoo.com>
2022-01-08 17:13:45 -0800

    new head

---
+++ build/precache-manifest.11a161536338d2025e278c521ba9e92e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e306cdb9688182d84db331877fbbb949",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fcfaf0933e5f859e9f0c",
+    "url": "/portfolio/static/css/2.30a2f84b.chunk.css"
+  },
+  {
+    "revision": "5df26b493043f6303b18",
+    "url": "/portfolio/static/css/main.5eb566e7.chunk.css"
+  },
+  {
+    "revision": "fcfaf0933e5f859e9f0c",
+    "url": "/portfolio/static/js/2.ed10b3d9.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/portfolio/static/js/2.ed10b3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df26b493043f6303b18",
+    "url": "/portfolio/static/js/main.222f614c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c679d8a082b3a49c2fca0951df473c7c",
+    "url": "/portfolio/static/media/Lights-Out.c679d8a0.png"
+  },
+  {
+    "revision": "833d87d7aaccb46ba4b5049ba852975e",
+    "url": "/portfolio/static/media/cccaa.833d87d7.PNG"
+  },
+  {
+    "revision": "82e9bc2b54efed11689a09e7de051415",
+    "url": "/portfolio/static/media/devmakers.82e9bc2b.jfif"
+  },
+  {
+    "revision": "95c7767da9fc98de8ab6e027ed5718db",
+    "url": "/portfolio/static/media/download.95c7767d.png"
+  },
+  {
+    "revision": "9c0c7971f0bea32b5e1b4c549564eefe",
+    "url": "/portfolio/static/media/everyonic.9c0c7971.jfif"
+  },
+  {
+    "revision": "01bab7d2bf1462b528c132cca27c6c4f",
+    "url": "/portfolio/static/media/omnifood.01bab7d2.png"
+  },
+  {
+    "revision": "951880c3305bad0867279107f2da985b",
+    "url": "/portfolio/static/media/pdfResume.951880c3.pdf"
+  },
+  {
+    "revision": "4c9e84f05c615822e3cde6a2c1822834",
+    "url": "/portfolio/static/media/resume-snippit.4c9e84f0.png"
+  },
+  {
+    "revision": "801afc30418be21741ad0892d9e969fd",
+    "url": "/portfolio/static/media/samuel_santibout.801afc30.pdf"
+  }
+]);
