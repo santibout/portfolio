e627e2a59bf7b45cfb7a2c6afc4b8090dfd4a096
Samuel Santibout <santibout@yahoo.com>
2022-11-09 17:19:10 -0800

    Updates

---
+++ precache-manifest.8c8446d8e7348967f19ceacf390d6786.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffde31d82d69079c56dda60681c6245",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab500e0d7a195016f8a",
+    "url": "/static/css/main.e4f61a07.chunk.css"
+  },
+  {
+    "revision": "e12f2b47839c4eb19363",
+    "url": "/static/js/2.4a69f50d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4a69f50d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab500e0d7a195016f8a",
+    "url": "/static/js/main.edbfe031.chunk.js"
+  },
+  {
+    "revision": "83640d1bf008095356ccca44c8877cd8",
+    "url": "/static/js/mapbox-gl-csp-worker.5c008ae2.worker.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "c679d8a082b3a49c2fca0951df473c7c",
+    "url": "/static/media/Lights-Out.c679d8a0.png"
+  },
+  {
+    "revision": "833d87d7aaccb46ba4b5049ba852975e",
+    "url": "/static/media/cccaa.833d87d7.PNG"
+  },
+  {
+    "revision": "82e9bc2b54efed11689a09e7de051415",
+    "url": "/static/media/devmakers.82e9bc2b.jfif"
+  },
+  {
+    "revision": "95c7767da9fc98de8ab6e027ed5718db",
+    "url": "/static/media/download.95c7767d.png"
+  },
+  {
+    "revision": "9c0c7971f0bea32b5e1b4c549564eefe",
+    "url": "/static/media/everyonic.9c0c7971.jfif"
+  },
+  {
+    "revision": "01bab7d2bf1462b528c132cca27c6c4f",
+    "url": "/static/media/omnifood.01bab7d2.png"
+  },
+  {
+    "revision": "951880c3305bad0867279107f2da985b",
+    "url": "/static/media/pdfResume.951880c3.pdf"
+  },
+  {
+    "revision": "4c9e84f05c615822e3cde6a2c1822834",
+    "url": "/static/media/resume-snippit.4c9e84f0.png"
+  },
+  {
+    "revision": "2302e5e025a36129511cbed3eb6da7a3",
+    "url": "/static/media/samuel_santibout.2302e5e0.pdf"
+  }
+]);
