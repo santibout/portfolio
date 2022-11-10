3775ef8bc21b58288b66c9fd66e6108afb99abc7
Samuel Santibout <santibout@yahoo.com>
2022-11-09 16:17:13 -0800

    Updates

---
+++ precache-manifest.a66e1e4288611882b454858165e8594a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66eaf727626dcc0acb51dfddd95a17b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ce1cf888a84bd6ef8e",
+    "url": "/static/css/main.e4f61a07.chunk.css"
+  },
+  {
+    "revision": "6d0d7088bffac40c8817",
+    "url": "/static/js/2.e1b889fb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e1b889fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ce1cf888a84bd6ef8e",
+    "url": "/static/js/main.ed22f353.chunk.js"
+  },
+  {
+    "revision": "83640d1bf008095356ccca44c8877cd8",
+    "url": "/static/js/mapbox-gl-csp-worker.5c008ae2.worker.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "c679d8a082b3a49c2fca0951df473c7c",
+    "url": "/static/media/Lights-Out.c679d8a0.png"
+  },
+  {
+    "revision": "833d87d7aaccb46ba4b5049ba852975e",
+    "url": "/static/media/cccaa.833d87d7.PNG"
+  },
+  {
+    "revision": "82e9bc2b54efed11689a09e7de051415",
+    "url": "/static/media/devmakers.82e9bc2b.jfif"
+  },
+  {
+    "revision": "95c7767da9fc98de8ab6e027ed5718db",
+    "url": "/static/media/download.95c7767d.png"
+  },
+  {
+    "revision": "9c0c7971f0bea32b5e1b4c549564eefe",
+    "url": "/static/media/everyonic.9c0c7971.jfif"
+  },
+  {
+    "revision": "01bab7d2bf1462b528c132cca27c6c4f",
+    "url": "/static/media/omnifood.01bab7d2.png"
+  },
+  {
+    "revision": "951880c3305bad0867279107f2da985b",
+    "url": "/static/media/pdfResume.951880c3.pdf"
+  },
+  {
+    "revision": "4c9e84f05c615822e3cde6a2c1822834",
+    "url": "/static/media/resume-snippit.4c9e84f0.png"
+  },
+  {
+    "revision": "2302e5e025a36129511cbed3eb6da7a3",
+    "url": "/static/media/samuel_santibout.2302e5e0.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e14fa34a067d2ef092292b15facb7ff4.js"
+  "/precache-manifest.a66e1e4288611882b454858165e8594a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
